c5b1cd874a1ee327e098ad8e39bf9132da1c35d7
Aron Banaś <138473770+banasaron@users.noreply.github.com>
2026-01-07 11:17:29 +0100

    add berlinclock method

--- src/main/java/org/example/BerlinClock.java
+++ src/main/java/org/example/BerlinClock.java
@@ -13,6 +13,10 @@ public class BerlinClock {
         this.minutes = arr[1];
         this.hours = arr[0];
     }
+    public String returnBerlinClock(){
+        return returnSeconds()+" "+returnHours()+" "+returnMinutes();
+    }
+
     private String returnSeconds(){
         return Integer.parseInt(this.seconds)%2==0 ? "Y" : "O";
     }
@@ -25,7 +29,52 @@ public class BerlinClock {
             hours-=5;
         }
         secondHours = hours%5;
-        String firstHoursString = 
+        StringBuilder firstHoursString = new StringBuilder();
+        StringBuilder secondHoursString = new StringBuilder();
+        for(int i=0; i<firstHours; i++){
+            firstHoursString.append("R");
+        }
+        for(int i=0; i<secondHours; i++){
+            secondHoursString.append("R");
+        }
+        for(int i=0; i<4-firstHours; i++){
+            firstHoursString.append("O");
+        }
+        for(int i=0; i<4-secondHours; i++){
+            secondHoursString.append("O");
+        }
+        return firstHoursString+" "+secondHoursString;
+    }
+    private String returnMinutes(){
+        int minutes = Integer.parseInt(this.minutes);
+        int firstMinutes = 0;
+        int secondMinutes = 0;
+        while(minutes>12){
+            firstMinutes++;
+            minutes-=12;
+        }
+        secondMinutes = minutes%12;
+        StringBuilder firstMinutesString = new StringBuilder();
+        StringBuilder secondMinutesString = new StringBuilder();
+        for(int i=0; i<firstMinutes; i++){
+            if(i%3==0){
+                firstMinutesString.append("Y");
+            }else{
+                firstMinutesString.append("R");
+            }
+
+
+        }
+        for(int i=0; i<secondMinutes; i++){
+            secondMinutesString.append("Y");
+        }
+        for(int i=0; i<12-firstMinutes; i++){
+            firstMinutesString.append("O");
+        }
+        for(int i=0; i<4-secondMinutes; i++){
+            secondMinutesString.append("O");
+        }
+        return firstMinutesString+" "+secondMinutesString;
     }
 
 }
